de3d964735b4311e32a8b25d0cf105fd1436e557
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2024-05-11 23:51:38 +0200

    Add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,25 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        olive: {
+          '50': '#f2f7f4',
+          '100': '#e0ebe3',
+          '200': '#c4d6ca',
+          '300': '#9cb9a8',
+          '400': '#709780',
+          '500': '#5a896f',
+          '600': '#3c5f4c',
+          '700': '#304c3e',
+          '800': '#283d32',
+          '900': '#21332a',
+          '950': '#121c18',
+        },
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
