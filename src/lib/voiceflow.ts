0f0e9780c9585926a8ee5ad36d791bc2add957af
Juan Carlos Quintero <juancarlosqr@users.noreply.github.com>
2024-05-29 09:44:08 -0400

    Add voiceflow  snippet

---
+++ src/lib/voiceflow.ts
@@ -0,0 +1,37 @@
+import { cookies } from 'next/headers'
+
+const vfProjectID = process.env.VF_PROJECT_ID
+const vfVersionID = process.env.VF_VERSION_ID
+const vfUserID = process.env.VF_USER_ID
+
+export async function generateVoiceflowScriptSnippet() {
+  const cookieStore = cookies()
+  let userID = cookieStore.get('userID')?.value
+
+  if (process.env.NODE_ENV !== 'production' && !userID && vfUserID) {
+    userID = vfUserID
+
+    console.log({ userID })
+  }
+
+  if (!vfProjectID || !vfVersionID || !userID) {
+    return
+  }
+
+  return {
+    __html: `<script type="text/javascript">
+          (function(d, t) {
+              var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+              v.onload = function() {
+                window.voiceflow.chat.load({
+                  verify: { projectID: '${vfProjectID}' },
+                  url: 'https://general-runtime.voiceflow.com',
+                  versionID: '${vfVersionID}',
+                  userID: '${userID}',
+                });
+              }
+              v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+          })(document, 'script');
+        </script>`,
+  }
+}
